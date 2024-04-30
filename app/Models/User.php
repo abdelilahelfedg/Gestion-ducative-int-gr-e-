08a9688bd080957ab4b9d5a92cde0e9adaf63ab0
Abdelilah <129623838+abdelilahelfedg@users.noreply.github.com>
2024-04-30 12:46:12 +0200

    Add files via upload

---
+++ app/Models/User.php
@@ -0,0 +1,27 @@
+<?php
+
+class User
+{
+    
+    use Model;
+    protected $table = 'login';
+    protected $allowedColumns = [ 'Email', 'password',];
+    
+    public function Validate($data){
+         $this->errors = [];
+         if(empty($data['Email'])){
+             $this->errors['Email'] = "email is required";
+        }else
+        if(!filter_var($data['Email'], FILTER_VALIDATE_EMAIL)){
+            $this->errors['Email'] = "email is not valid";
+        } 
+        if(empty($data['password'])){
+            $this->errors['password'] = "password is required";
+       }
+         if(empty($this->errors)){
+            return true;
+         }
+         return false;
+    }
+}
+
