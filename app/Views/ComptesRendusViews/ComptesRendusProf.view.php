1725bab4f58a04aa3d9736b8809bfe2651056396
nwiuser <152625420+nwiuser@users.noreply.github.com>
2024-05-26 03:05:32 +0100

    Add files via upload

--- app/Views/ComptesRendusViews/ComptesRendusProf.view.php
+++ app/Views/ComptesRendusViews/ComptesRendusProf.view.php
@@ -3,67 +3,150 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Espace Comptes Rendus Prof</title>
+    <title>Admin Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
-</head>
+<style>
+  .card {
+  box-sizing: border-box;
+  width: 600px;
+  height: 270px;
+  background: rgba(217, 217, 217, 0.58);
+  border: 1px solid white;
+  box-shadow: 12px 17px 51px rgba(0, 0, 0, 0.22);
+  backdrop-filter: blur(6px);
+  border-radius: 17px;
+  text-align: center;
+  cursor: pointer;
+  transition: all 0.5s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  user-select: none;
+  font-weight: bolder;
+  color: black;
+}
+
+@media (max-width: 1405px) {
+  .card {
+    width: 315px;
+  }
+}
+
+@media (max-width: 900px) {
+  .card {
+    width: 460px;
+  }
+}
+.card:hover {
+  border: 1px solid black;
+  transform: scale(1.05);
+}
+
+.card:active {
+  transform: scale(0.95) rotateZ(1.7deg);
+}
+body {
+  background-image: url('../public/assets/images/profbg.png');
+  background-size: cover; 
+  background-position: center; 
+  background-repeat: no-repeat;
+}
+.icon{
+  padding: 20px; 
+}
+h3 a {
+  text-decoration: none; 
+  color: inherit;
+}
+img{
+  width: 70px;
+  height: 60px;
+  color: black;
+}
+.row{
+  text-align: center;
+  margin-bottom: -100px;
+}
+nav{
+  box-shadow: 12px 17px 51px rgba(0, 0, 0, 0.22);
+}
+.nav-link {
+  color: black;
+}
+button {
+  color: #090909;
+  padding: 0.7em 1.7em;
+  font-size: 18px;
+  border-radius: 0.5em;
+  background: #e8e8e8;
+  cursor: pointer;
+  border: 1px solid #e8e8e8;
+  transition: all 0.3s;
+  /* box-shadow: 6px 6px 12px #c5c5c5, -6px -6px 12px #ffffff; */
+  margin: 5px;
+}
+
+</style>
+
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary p-5">
+
+<nav class="navbar navbar-expand-lg navbar-dark shadow-5-strong p-4">
+
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+
+    <a class="navbar-brand" href=""><img src="data:image/jpeg;base64,<?= base64_encode(file_get_contents("../public/assets/images/t2.png")) ?>" alt="ENSAH"></a>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
+          <a class="nav-link" href="#"><strong>Log out</strong></a>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    
     </div>
+
   </div>
+
 </nav>
 
-
-<div class="container my-5 ">
+<div class="container my-5">
     <div class="row pt-5 mb-5">
-        <div class="col-md-4">
-            <div class="card p-3 mb-2 ">
-                <a href="<?= ROOT . "/" . "ComptesRendusControl" . "/" . "Definir"?>" class="btn btn-lg btn-primary border border-0">Definir un Deadline</a>
-                
+        <div class="col-md-6">
+            <h3><a href="<?= ROOT . "/" . "ComptesRendusControl" . "/" . "Definir"?>"></a>
+            <div class="card p-3 mb-2 "><i class="fas fa-calendar-alt icon fa-3x"></i>
+            Definir un Deadline
+                </div>
+              </a></h3>
             </div>
-        </div>
         
-        <div class="col-md-4">
+        <div class="col-md-6"> <!-- à voir avec abdelilah -->
             <div class="card p-3 mb-2">
-              <?php if(isset($_GET['message'])){?>
-              <div class="card-body">
-               <div class="container mt-5">
-                  <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <strong>Il faut choisir une option.</strong> 
-                  </div>
-                </div> 
-                <?php }?>
-                <h5 class="card-title">Select Module</h5>
+                <h5>Choisir un Module</h5>
                 <form action="<?= ROOT ."/" ."ComptesRendusControl/" ."Acceder" ?>" method="POST">
                     <div class="form-group">
                         <select name="Module" class="form-control" id="exampleFormControlSelect1">
-                            <option >Choisissez une option</option>
+                            <option>Choisissez une option</option>
                             <?php for($i = 0; $i < count($data); $i ++):?>
                                 <option><?= $data[$i];?></option>
                             <?php endfor; ?>
                         </select>
                     </div>
-                    <button id="validationBtn" class="btn btn-primary">Acces Comptes Rendus</button>
+                    <button>Acces Comptes Rendus</button>
                 </form>
                 
               </div>
@@ -72,9 +155,6 @@
 
       </div>
 </div>
-
-
-
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- app/Views/ComptesRendusViews/DefinirDeadline.view.php
+++ app/Views/ComptesRendusViews/DefinirDeadline.view.php
@@ -3,44 +3,168 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Partage des Annonces</title>
+    <title>Définir un Deadline</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+
+.container{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 200px;
+}
+.form {
+  background-color: #fff;
+  box-shadow: 0 10px 60px rgb(0, 0, 0);
+  border: 1px solid rgb(159, 159, 160);
+  border-radius: 20px;
+  padding: 2rem .7rem .7rem .7rem;
+  text-align: center;
+  font-size: 1.125rem;
+  width: 550px;
+}
+
+.form-title {
+  color: #000000;
+  font-size: 1.8rem;
+  font: Montserrat, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", Segoe UI Symbol, "Noto Color Emoji" !important;
+  font-weight: 500;
+}
+
+.form-paragraph {
+  margin-top: 10px;
+  font-size: 0.9375rem;
+  color: rgb(105, 105, 105);
+}
+
+.drop-container {
+  background-color: #fff;
+  position: relative;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  margin-top: 2.1875rem;
+  border-radius: 10px;
+  border: 2px dashed rgb(171, 202, 255);
+  color: #444;
+  cursor: pointer;
+  transition: background .2s ease-in-out, border .2s ease-in-out;
+  display: flex;
+}
+
+.drop-container:hover {
+  background: rgba(0, 140, 255, 0.164);
+  border-color: rgba(17, 17, 17, 0.616);
+}
+
+.drop-container:hover .drop-title {
+  color: #222;
+}
+
+.drop-title {
+  color: #444;
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  transition: color .2s ease-in-out;
+}
+
+#file-input {
+  width: 350px;
+  max-width: 100%;
+  color: #444;
+  padding: 2px;
+  background: #fff;
+  border-radius: 10px;
+  border: 1px solid rgba(8, 8, 8, 0.288);
+}
+
+#file-input::file-selector-button {
+  margin-right: 20px;
+  border: none;
+  background: #084cdf;
+  padding: 10px 20px;
+  border-radius: 10px;
+  color: #fff;
+  cursor: pointer;
+  transition: background .2s ease-in-out;
+}
+
+#file-input::file-selector-button:hover {
+  background: #0d45a5;
+}
+
+.input {
+  border: none;
+  outline: none;
+  border-radius: 15px;
+  padding: 1em;
+  margin-left: 15px;
+  background-color: #ccc;
+  box-shadow: inset 2px 5px 10px rgba(0,0,0,0.1);
+  transition: 300ms ease-in-out;
+}
+select{
+  width: 260px;
+}
+
+button {
+  --hover-shadows: 5px 5px 8px #121212, -5px -5px 8px #303030;
+  --accent: fuchsia;
+  font-weight: bold;
+  border: none;
+  border-radius: 1.1em;
+  background-color:  #004225;
+  cursor: pointer;
+  color: white;
+  width: 490px;
+  height: 50px;
+  margin-top: 10px;
+  transition: box-shadow ease-in-out 0.3s, background-color ease-in-out 0.1s, letter-spacing ease-in-out 0.1s, transform ease-in-out 0.1s;
+}
+
+button:hover {
+  box-shadow: var(--hover-shadows);
+}
+body {
+  background-image: url('../public/assets/images/profbg.png');
+  background-size: cover; 
+  background-position: center; 
+  background-repeat: no-repeat;
+}
+
+    </style>
 </head>
 <body>
 
-<div class="container-lg bg-primary my-5">
-        <div class="row justify-content-center">
-            <div class="col-5">
-                <div class="card bg-light my-5">
-                    <div class="card-body">
-                        <div class="card-title text-center mb-3">Definition du deadline</div>
-                        <form method="POST" enctype="multipart/form-data">
-                            <label for="dead" class="form-label">Saisir Deadline</label>
-                            <input type="date" name="Date_f" id="dead" class="form-control mb-3" placeholder="Date" required>
-                            <input type="time" name="Time_f" id="dead" class="form-control mb-3" placeholder="Time" required>
-                            
-                            <div class="form-floating">
-                                <select name="Module" class="form-select mb-3" id="floatingSelect" aria-label="Floating label select example">
-                                    <?php for($i = 0; $i < count($data); $i ++):?>
-                                       <option ><?= $data[$i];?></option>
-                                    <?php endfor; ?>
-                                     
-                                </select>
-                                <label for="floatingSelect">Select Module</label>
-                            </div>
-                           
-                            <button class="btn btn-primary my-3">Definir</button>
-                       </form>
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container">
+<form class="form">
+  <span class="form-title">Definition du Deadline</span>
+  <div class="form-paragraph">
+    <label for="email"><strong>Saisir un deadline</strong></label>
+    <input type="date" name="Date_f" id="dead" class="form-control mb-3" placeholder="Date" required>
+    <input type="time" name="Time_f" id="dead" class="form-control mb-3" placeholder="Time" required>
+  </div>
+  <div class="form-paragraph">
+    <label for="email"><strong>Choisir un Module</strong></label>
+    <select name="Module" class="input">
+        <?php for($i = 0; $i < count($data); $i ++):?>
+            <option ><?= $data[$i];?></option>
+        <?php endfor; ?>
+    </select>
+  </div>
+</label>
+<button>Définir</button>
+<a href="<?= ROOT."/"."HomeProf"?>"><button>Retour</button></a>
+</form>
+</div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
