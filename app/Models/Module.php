dcc7f7afabc298f82bf3b9ffd9691f443d01898c
Abdelilah <129623838+abdelilahelfedg@users.noreply.github.com>
2024-05-30 11:29:35 +0100

    Update Module.php

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -1,30 +1,8 @@
 <?php
 class Module{
    use Model;
-   protected $table = 'module';
-   protected $allowedColumns = [ 'id_module', 'nom', 'filière', 'email_prof_cour','email_prof_tp'];
+   protected $table = 'modules';
+//   protected $allowedColumns = [ 'id_module', 'nom', 'Niveau', 'email_prof_cours','email_prof_tp'];
    
-   public function Validate($data){
-        $this->errors = [];
-        if(empty($data['Email'])){
-            $this->errors['Email'] = "email is required";
-       }else
-       if(!filter_var($data['Email'], FILTER_VALIDATE_EMAIL)){
-           $this->errors['Email'] = "email is not valid";
-       } 
-       if(empty($data['password'])){
-           $this->errors['password'] = "password is required";
-      }
-        if(empty($this->errors)){
-           return true;
-        }
-        return false;
-   }
-
-
 }
-
-
-
-
 ?>
