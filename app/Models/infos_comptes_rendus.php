3e4c222b7715631052996591c820c14f27259508
Abdelilah <129623838+abdelilahelfedg@users.noreply.github.com>
2024-05-30 11:28:42 +0100

    Delete app/Models/infos_comptes_rendus.php

--- app/Models/infos_comptes_rendus.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class infos_comptes_rendus{
-    use Model;
-    protected $table = "Infos_comptes_rendus";
-    protected $allowedColumns = ['Prof', 'Module', 'time_d', 'date_f', 'time_f'];
-}
