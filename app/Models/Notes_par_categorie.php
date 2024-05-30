df99fb5ce715eaeeb02525447f43abe0e97403aa
Abdelilah <129623838+abdelilahelfedg@users.noreply.github.com>
2024-05-30 11:30:57 +0100

    Update Notes_par_categorie.php

--- app/Models/Notes_par_categorie.php
+++ app/Models/Notes_par_categorie.php
@@ -1,1 +1,20 @@
+<?php
 
+
+class Notes_par_categorie
+{
+    use Model;
+
+    protected $table = 'Notes';
+    protected $categorie;
+
+    protected $allowedColumns;
+
+    public function __construct($categrie = null)
+    {
+        $this->categorie = $categrie;   
+        $this->allowedColumns = [ 'Etudiant', 'Module', 'Niveau', $this->categorie];
+    }
+    
+    
+}
