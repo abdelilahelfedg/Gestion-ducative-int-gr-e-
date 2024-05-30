b23128afe333e2bb679214684e8b25feb354f09d
Abdelilah <129623838+abdelilahelfedg@users.noreply.github.com>
2024-05-30 11:21:09 +0100

    Update Annonces.php

--- app/Models/Annonces.php
+++ app/Models/Annonces.php
@@ -7,20 +7,5 @@ class Annonces
     protected $table = 'Annonces';
     protected $allowedColumns = [ 'Objet', 'File', 'Niveau', 'Prof'];
     
-    public function Validate($data){
-         $this->errors = [];
-         if(empty($data['Email'])){
-             $this->errors['Email'] = "email is required";
-        }else
-        if(!filter_var($data['Email'], FILTER_VALIDATE_EMAIL)){
-            $this->errors['Email'] = "email is not valid";
-        } 
-        if(empty($data['password'])){
-            $this->errors['password'] = "password is required";
-       }
-         if(empty($this->errors)){
-            return true;
-         }
-         return false;
-    }
+    
 }
