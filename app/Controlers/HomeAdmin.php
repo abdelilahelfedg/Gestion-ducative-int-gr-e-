4503650106474c4773e1c188109865c6b567f5ea
Abdelilah <129623838+abdelilahelfedg@users.noreply.github.com>
2024-05-30 11:17:19 +0100

    Update HomeAdmin.php

--- app/Controlers/HomeAdmin.php
+++ app/Controlers/HomeAdmin.php
@@ -1,1 +1,14 @@
+<?php
 
+class HomeAdmin
+{
+    use Controller;
+    public function index(){
+        if(!isset($_SESSION['USER'])){
+            
+            header('Location: '. ROOT);
+        }
+        // $obj = new Etudiant;
+        $this->view('Admin');
+    }
+}
