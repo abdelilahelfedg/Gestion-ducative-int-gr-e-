967fd974af9e0ebb063a8a6dbbedf52d54513ddc
Abdelilah <129623838+abdelilahelfedg@users.noreply.github.com>
2024-05-30 11:19:30 +0100

    Update HomeProf.php

--- app/Controlers/HomeProf.php
+++ app/Controlers/HomeProf.php
@@ -1,1 +1,15 @@
+<?php
+
+class HomeProf
+{
+    use Controller;
+    public function index(){
+        if(!isset($_SESSION['USER'])){
+            
+            header('Location: '. ROOT);
+        }
+        // $obj = new Etudiant;
+        $this->view('Prof');
+    }
+}
 
