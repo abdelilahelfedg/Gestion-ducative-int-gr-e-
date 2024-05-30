2db2ea6117bac9c2ec4adfb4b20ccfaed3c7281a
Abdelilah <129623838+abdelilahelfedg@users.noreply.github.com>
2024-05-30 11:18:14 +0100

    Update HomeEtd.php

--- app/Controlers/HomeEtd.php
+++ app/Controlers/HomeEtd.php
@@ -1,1 +1,15 @@
+<?php
+
+class HomeEtd
+{
+    use Controller;
+    public function index(){
+        if(!isset($_SESSION['USER'])){
+            
+            header('Location: '. ROOT);
+        }
+        
+        $this->view('Etudiant');
+    }
+}
 
