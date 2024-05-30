b423cc3c54a5751474a010fddcc54e65dd23d97c
Abdelilah <129623838+abdelilahelfedg@users.noreply.github.com>
2024-05-30 11:34:35 +0100

    Update annoncesAdmin.view.php

--- app/Views/AnnoncesViews/annoncesAdmin.view.php
+++ app/Views/AnnoncesViews/annoncesAdmin.view.php
@@ -7,9 +7,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-
-<div class="container-lg bg-primary my-5">
+<body class='bg-primary'>
+<nav class="navbar navbar-expand-lg bg-body-tertiary p-5">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="<?= ROOT . "/HomeAdmin"?>"><strong>Ensah</strong></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="<?= ROOT . "/logout"?>">Log out</a>
+        </li>
+      </ul>
+    </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+       
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+<div class="container my-5">
         <div class="row justify-content-center">
             <div class="col-5">
                 <div class="card bg-light my-5">
@@ -31,6 +51,12 @@
         </div>
     </div>
 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
